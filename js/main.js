b795ad10507fe019d808dcbc0e2d1616ea152ce6
osamaakhan61 <osamaa.khan61@gmail.com>
2021-02-24 10:57:34 -0800

    Added next button and hid query screen once clicking next #1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ var $gstart = document.querySelector('#gstart');
 var $body = document.querySelector('.start-page');
 var $divPage1 = document.querySelector('.row');
 var $divPage2 = document.querySelector('.page2-row');
+var $divPage3 = document.querySelector('.page3');
 var $centuryButtons = document.querySelectorAll('.century');
+var $nextButton = document.querySelector('.next-button');
 
 $gstart.addEventListener('click', changePage);
 
@@ -17,8 +19,10 @@ $centuryButtons.forEach(item => {
   item.addEventListener('click', event => {
     if (item.className === 'century') {
       item.className = 'century-onclick';
+      $nextButton.className = 'next-button';
     } else {
       item.className = 'century';
+      $nextButton.className = 'hidden';
     }
   });
 });
@@ -27,4 +31,19 @@ if (data.view === 'page2') {
   $body.className = 'page2-bg';
   $divPage1.className = 'hidden row';
   $divPage2.className = 'page2-row';
+}
+
+$nextButton.addEventListener('click', changePage2);
+
+function changePage2(event) {
+  $body.className = 'app-bg';
+  $divPage2.className = 'hidden page2-row';
+  $divPage3.className = 'page3';
+  data.view = 'page3';
+}
+
+if (data.view === 'page3') {
+  $body.className = 'app-bg';
+  $divPage2.className = 'hidden page2-row';
+  $divPage3.className = 'page3';
 }
