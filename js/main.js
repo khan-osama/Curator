a6aa5ede7fae8e6aa6e1641a559d5c3a2d0f997e
osamaakhan61 <osamaa.khan61@gmail.com>
2021-02-25 10:36:42 -0800

    Create DOM Skeleton in JS #2

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ var $centuryButtons = document.querySelectorAll('.century');
 var $nextButton = document.querySelector('.next-button');
 var $artButton = document.querySelector('#next');
 var $backButton = document.querySelector('#previous');
+var $artList = document.querySelector('art-container-list');
 
 $gstart.addEventListener('click', changeToArtPeriod);
 
@@ -74,8 +75,35 @@ if (data.view === 'add-art-page') {
   $divPage4.className = 'add-art-page';
 }
 
-/* function createArtPieces(event) {
-  var newLi = document.createElement('li');
-  var newIMG = document.createElement('img');
+function createArtPieces(event) {
+  var newDivRow = document.createElement('div');
 
-} */
+  newDivRow.className = 'art-adding-page-row';
+
+  /* newArtImg.setAttribute('src', data.img); */
+
+  /* newArtArtist.textContent = data.artistName; */
+  /* newItalics.textContent = data.artTitle;
+  newArtTitleYear.textContent = data.artYear; */
+
+  $artList.appendChild(newDivRow);
+  for (var i = 0; i < 2; i++) {
+    var newDivColumnHalf = document.createElement('div');
+    var newLi = document.createElement('li');
+    var newArtImg = document.createElement('img');
+    var newArtArtist = document.createElement('h2');
+    var newArtTitleYear = document.createElement('p');
+    var newItalics = document.createElement('i');
+
+    newDivColumnHalf.className = 'column-half';
+    newArtArtist.className = 'artist-name-header';
+    newArtTitleYear.className = 'art-name-year';
+
+    newDivRow.appendChild(newDivColumnHalf);
+    newDivColumnHalf.appendChild(newLi);
+    newLi.appendChild(newArtImg);
+    newLi.appendChild(newArtArtist);
+    newLi.appendChild(newArtTitleYear);
+    newArtTitleYear.appendChild(newItalics);
+  }
+}
